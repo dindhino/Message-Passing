192e1e8b572c1f5de5a50694c0d62636b4113878
Dindin Dhino Alamsyah <dindhinoo@gmail.com>
2017-03-29 01:59:56 +0700

    Add files via upload

---
+++ MessagePassing_1301144360_1301140240/src/ProducerGoodMorning.java
@@ -0,0 +1,26 @@
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ProducerGoodMorning {
+
+    private final static String QUEUE_NAME = "greeting";
+
+    public static void main(String[] argv) throws Exception {
+
+        for (int i = 1; i <= 10; i++) {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("127.0.0.1");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "good morning!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+
+            channel.close();
+            connection.close();
+        }
+    }
+}
